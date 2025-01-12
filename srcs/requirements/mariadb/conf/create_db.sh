0b02ab6d224283237b761594243802a127341065
Abdellahbounab <Abdellahbounab@gmail.com>
2025-01-12 11:59:42 -0500

    updating secrets

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
+
+MYSQL_ROOT=$(cat /run/secrets/db_root_password)
+MYSQL_PASS=$(cat /run/secrets/db_password)
+
 	echo "Creating database..."
 	cat << EOF > /tmp/create_db.sql
 USE mysql;
@@ -11,6 +15,7 @@ CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';
 GRANT ALL PRIVILEGES ON ${MYSQL_NAME}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
+
 	echo "Database created"
 	mariadbd --user=mysql --bootstrap < /tmp/create_db.sql
 	rm -f /tmp/create_db.sql
--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -2,6 +2,13 @@
 
 #install wp command for setup the wordpress to the data base
 if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
+
+
+	WORDPRESS_PASS=$(awk -F'=' '/WORDPRESS_PASS/ {printf "%s", $2}' /run/secrets/credientials)
+	USER_PASS=$(awk -F'=' '/USER_PASS/ {printf "%s", $2}' /run/secrets/credientials)
+	MYSQL_PASS=$(cat /run/secrets/db_password)
+
+
 	echo "installing wp-cli"
 	curl -OL https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
@@ -12,12 +19,12 @@ if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
 
 	cd /var/www/html/wordpress
 
-	wp config create --dbname=${MYSQL_NAME} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASS} --dbhost="mariadb"  --allow-root
+	wp config create --dbname=${MYSQL_NAME} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASS} --dbhost=${MYSQL_HOST}  --allow-root
 	wp core install --url="${DOMAIN_NAME}" --title="Inception" --admin_user=${WORDPRESS_ADMIN} --admin_password=${WORDPRESS_PASS} --admin_email="${WORDPRESS_ADMIN}@example.com"  --allow-root
 	wp theme install twentytwentythree  --allow-root
 	wp theme activate twentytwentythree  --allow-root
 
-	wp user create ${MYSQL_USER} ${MYSQL_USER}@example.com --user_pass=${MYSQL_PASS} --role=subscriber --allow-root
+	wp user create ${MYSQL_USER} ${MYSQL_USER}@example.com --user_pass=${USER_PASS} --role=subscriber --allow-root
 	
 	chown -R www-data:www-data /var/www/html/wordpress
 	chmod -R 755 /var/www/html/wordpress
